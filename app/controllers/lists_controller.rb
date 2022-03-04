c698932a79a58b793a7821fb6af955f776e15363
Brianna Foley <brianna.foley87@gmail.com>
2022-03-04 17:00:15 -0600

    complete creating the list and bookmark features

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,5 +4,23 @@ class ListsController < ApplicationController
   end
 
   def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
